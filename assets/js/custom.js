1abfaf92ed029173976641618729da1ebe16fd48
Czar22Ke <157858076+Czar22Ke@users.noreply.github.com>
2024-06-27 08:57:11 +0100

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -27,7 +27,7 @@
       hour = minute * 60,
       day = hour * 24;
 
-	let countDown = new Date('Jan 30, 2024 10:30:00').getTime(),
+	let countDown = new Date('Jun 30, 2024 10:30:00').getTime(),
     x = setInterval(function() {    
 
       let now = new Date().getTime(),
